f7c0fe6ba6557621b1c642d78d18f00452c1d0e7
Omji Awasthi <officialhindu56@gmail.com>
2025-12-17 15:38:01 +0530

    Update StudentList.js

--- frontend/src/components/StudentList.js
+++ frontend/src/components/StudentList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://studentmanagement-1-zjez.onrender.com/api';
 
 function StudentList() {
   const [students, setStudents] = useState([]);
