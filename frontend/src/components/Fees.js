8634a6feefea76ab135ffc7f8d7497b7a15f3c02
Omji Awasthi <officialhindu56@gmail.com>
2025-12-17 15:37:10 +0530

    Update Fees.js

--- frontend/src/components/Fees.js
+++ frontend/src/components/Fees.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://studentmanagement-1-zjez.onrender.com/api';
 
 function Fees() {
   const [students, setStudents] = useState([]);
